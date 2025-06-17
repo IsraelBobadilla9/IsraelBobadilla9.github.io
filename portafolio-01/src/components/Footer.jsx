7ec747851bc73f672e2f6d71e00c6ca6a96e49b3
IsraelBobadilla9 <israelbobadilla9@hotmail.com>
2025-06-16 21:50:25 -0600

    Portafolio

---
+++ portafolio-01/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () =>{
+    return (
+        <footer>
+            {" "}
+            <p>&copy; {new Date().getFullYear()} Pedrotech.co. All rights reserved.</p>
+        </footer>
+    )
+}
